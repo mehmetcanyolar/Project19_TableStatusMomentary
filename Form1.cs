3dc70383bbf361f66786013abfd9ba3bb7cd2489
mehmet can yolar <mehmetcanyolar@gmail.com>
2024-12-09 23:24:12 +0300

    09/12/24--- timer-led , instant status of tables

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,63 @@ namespace Project19_TableStatusMomentary
         {
             InitializeComponent();
         }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            timer1.Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Db19Project20Entities context = new Db19Project20Entities();
+            var buttons = this.Controls.OfType<Button>(); // filtreleme işlemi
+            foreach (var itemButton in buttons)
+            {
+                this.Controls.Remove(itemButton);  //Eski masa buttonlerını silip her seferinde güncel masa bilgilerini getirir
+            }
+
+
+            var values = context.TblTables.ToList();
+
+
+            int buttonWidth = 100;
+            int buttonHeight = 50;
+            int padding = 10;
+            int xOffset = 10;
+            int yOffset = 10;
+
+            for (int i = 0; i < values.Count; i++)
+            {
+                var item = values[i];
+                Button button = new Button();
+                button.Text = item.TableNumber.ToString();
+                button.Size = new Size(buttonWidth, buttonHeight);
+                button.Location = new Point(xOffset + (i % 4) * (buttonWidth + padding), yOffset + (i / 4) * (buttonHeight + padding));
+
+
+
+
+
+
+                button.BackColor = bool.Parse(item.Status.ToString()) ? System.Drawing.Color.Green : System.Drawing.Color.Red;
+
+
+                //if (item.Status == true)
+                //{
+                //    button.BackColor = Color.Green;
+                //}
+                //else
+                //{
+                //    button.BackColor = Color.Red;
+                //}
+
+
+                this.Controls.Add(button);
+            }
+        }
     }
 }
+
